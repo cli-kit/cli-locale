8aad868d55c3d02b0594d7167f575e24a080765c
cyberfunk <hello@cyberfunk.org>
2014-01-28 21:22:53 +0800

    Update README.

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # Locale
 
+Utilities for working with LC environment variables
+
+## API
+
+### sanitize(lang)
+
+Sanitize the value of an LC variable removing any character encoding portion, such that en_GB.UTF-8 becomes en_gb.
+
+* `lang`: A language identifier extracted from an LC environment variable.
+
+Returns a sanitized language identifier.
+
+### find(search)
+
+Find the value of an LC environment variable and return a sanitized represention of the locale. If no variable value is found in the search array then this method returns the first available LC variable.
+
+* `search`: Array of LC environment variables to prefer.
+
+Return a language identifier.
+
 ## License
 
 Everything is [MIT](http://en.wikipedia.org/wiki/MIT_License). Read the [license](/LICENSE) if you feel inclined.
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+var language = 'en';
+
 /**
  *  Sanitize the value of an LC variable removing any character
  *  encoding portion, such that en_GB.UTF-8 becomes en_gb.
  *
  *  @param lang A language identifier extracted from an LC variable.
+ *
+ *  @return A sanitized language identifier.
  */
 function sanitize(lang) {
+  if(!lang) return lang;
   return lang.replace(/\..*$/, '').toLowerCase();
 }
 
@@ -16,24 +21,30 @@ function sanitize(lang) {
  *  method returns the first available LC variable.
  *
  *  @param search An array of LC variables to prefer.
+ *
+ *  @return A language identifier.
  */
 function find(search) {
-  var lang, search = config.lc || [], i, k, v, re = /^LC_/;
+  var lang = language, search = config.lc || [], i, k, v, re = /^LC_/;
   for(i = 0;i < search.length;i++) {
     lang = sanitize(process.env[process.env[search[i]]] || '');
     if(lang) return lang;
   }
   // nothing found in search array, find first available
-  if(!lang) {
-    for(k in process.env) {
-      v = process.env[k];
-      if(re.test(k) && v) {
-        lang = sanitize(v);
-      }
+  for(k in process.env) {
+    v = process.env[k];
+    if(re.test(k) && v) {
+      lang = sanitize(v);
     }
   }
   return lang;
 }
 
+module.exports = function(lang) {
+  language = lang;
+  return module.exports;
+}
+
 module.exports.sanitize = sanitize;
 module.exports.find = find;
+module.exports.language = language;
